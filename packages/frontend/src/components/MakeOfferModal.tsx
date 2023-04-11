429c9ae0e7c1c41f7b936c4f91ee2b5c95cdec4a
CJ Rose <cjrose59@gmail.com>
2023-04-11 14:03:36 -0700

    prove scores for offer

--- packages/frontend/src/components/MakeOfferModal.tsx
+++ packages/frontend/src/components/MakeOfferModal.tsx
@@ -13,16 +13,34 @@ type Props = {
   setShowMakeOffer: (value: boolean) => void;
 }
 
+type ProofInfo = {
+  publicSignals: string[]
+  proof: string[]
+  valid: boolean
+}
+
 export default observer(({ listingId, section, setShowMakeOffer }: Props) => {
   const app = React.useContext(Trustlist)
   const user = React.useContext(User)
 
+  const [proveData, setProveData] = React.useState<{
+    [key: number]: number | string
+  }>({})
+  const [repProof, setRepProof] = React.useState<ProofInfo>({
+      publicSignals: [],
+      proof: [],
+      valid: false,
+  })
   const [offerAmount, setOfferAmount] = React.useState('')
   const [score1, setScore1] = React.useState('')
   const [score2, setScore2] = React.useState('')
   const [score3, setScore3] = React.useState('')
   const [score4, setScore4] = React.useState('')
   const responderId = '12345'
+
+  if (!user.userState) {
+    return <div className="container">Loading...</div>
+  }
 
   return (
     <div className='dark-bg'>
@@ -42,7 +60,44 @@ export default observer(({ listingId, section, setShowMakeOffer }: Props) => {
                           className='offer-input'
                         />
                     </div>
-                    <div>
+
+                    {Array(
+                      user.userState.sync.settings.sumFieldCount
+                    )
+                      .fill(0)
+                      .map((_, i) => {
+                          return (
+                              <div key={i}>
+                                  <label htmlFor={`score${i +1}`}>prove Score {i + 1}</label>
+                                  <input
+                                      // style={{width: '6rem'}}
+                                      className='offer-input'
+                                      type='text'
+                                      id={`score${i + 1}`}
+                                      name={`score${i + 1}`}
+                                      value={proveData[i] ?? '0'}
+                                      onChange={(event) => {
+                                          if (
+                                              !/^\d*$/.test(
+                                                  event.target.value
+                                              )
+                                          )
+                                              return
+                                          setProveData(() => ({
+                                              ...proveData,
+                                              [i]: event.target.value,
+                                          }))
+                                          if (i === 0) {setScore1(event.target.value)}
+                                          if (i === 1) {setScore2(event.target.value)}
+                                          if (i === 2) {setScore3(event.target.value)}
+                                          if (i === 3) {setScore4(event.target.value)}
+                                      }}
+                                  />
+                              </div>
+                          )
+                      })}
+
+                    {/* <div>
                         <label htmlFor='score1'>reveal score1</label>
                         <input 
                           type='text' 
@@ -81,10 +136,38 @@ export default observer(({ listingId, section, setShowMakeOffer }: Props) => {
                           onChange={(e) => setScore4(e.target.value)}
                           className='offer-input'
                         />
-                      </div>
+                      </div> */}
+              </div>
+
+              <div className='offer-buttons'>
+                <Button
+                  onClick={async () => {
+                    const proof = await user.proveData(
+                        proveData
+                    )
+                    setRepProof(proof)
+                  }}
+                >
+                  prove trust scores
+                </Button>
+                {repProof.proof.length ? (
+                  <>
+                    {repProof.valid ? '✅' : '❌'}
+                  </>
+                ) : null}
+                {repProof.valid ? (
+                  <input 
+                    style={{marginTop: '1rem'}}
+                    type='submit'
+                    value='submit offer'
+                    onClick={() => app.submitOffer(listingId, section, responderId, offerAmount, score1, score2, score3, score4)}
+                  />
+                ) : (
+                    <button style={{marginTop: '1rem'}} className='blocked'>submit offer</button>
+                )}
               </div>
               
-              <div className='offer-buttons'>
+              {/* <div className='offer-buttons'>
                   <Button>prove trust scores</Button>
                   <button 
                     style={{marginTop: '1rem'}}
@@ -93,7 +176,7 @@ export default observer(({ listingId, section, setShowMakeOffer }: Props) => {
                   >
                     submit offer
                   </button>
-              </div>
+              </div> */}
             </div>
 
                 
