9ddbbe3a327ce035c3f05603b24d637c55a3079e
CJ Rose <cjrose59@gmail.com>
2023-04-11 12:44:15 -0700

    prove scores for listing

--- packages/frontend/src/components/NewListingModal.tsx
+++ packages/frontend/src/components/NewListingModal.tsx
@@ -11,23 +11,47 @@ type Props = {
   setShowNewListing: (value: boolean) => void;
 }
 
+type ProofInfo = {
+  publicSignals: string[]
+  proof: string[]
+  valid: boolean
+}
+
 export default observer(({ setShowNewListing }: Props) => {
   const app = React.useContext(Trustlist)
   const user = React.useContext(User)
 
+  const [proveData, setProveData] = React.useState<{
+    [key: number]: number | string
+  }>({})
+  const [repProof, setRepProof] = React.useState<ProofInfo>({
+      publicSignals: [],
+      proof: [],
+      valid: false,
+  })
   const [section, setSection] = React.useState('for sale')
   const [category, setCategory] = React.useState('')
   const [title, setTitle] = React.useState('')
-  const [amount, setamount] = React.useState('')
-  const [amountType, setamountType] = React.useState('one time')
+  const [amount, setAmount] = React.useState('')
+  const [amountType, setAmountType] = React.useState('one time')
   const [description, setDescription] = React.useState('')
   const [score1, setScore1] = React.useState('')
   const [score2, setScore2] = React.useState('')
   const [score3, setScore3] = React.useState('')
   const [score4, setScore4] = React.useState('')
-  const [repProofInputs, setRepProofInputs] = React.useState({})
-  const [repProof, setRepProof] = React.useState(null)
   const posterId = '09876'
+
+  const fieldType = (i: number) => {
+    if (i < user.sumFieldCount) {
+        return 'sum'
+    } else if (i % 2 === user.sumFieldCount % 2) {
+        return 'replace'
+    } else return 'timestamp'
+  }
+
+  if (!user.userState) {
+    return <div className="container">Loading...</div>
+  }
 
   return (
     <div 
@@ -88,13 +112,13 @@ export default observer(({ setShowNewListing }: Props) => {
                         type='text' 
                         id='amount' 
                         name='amount'
-                        onChange={(e) => setamount(e.target.value)} 
+                        onChange={(e) => setAmount(e.target.value)} 
                         className='form-input-sm'
                       />
                     </div>
                     <div className='form-flex'>
                       <label htmlFor='amountType'>amount type</label>
-                      <select id='amountType' name='amountType' onChange={(e) => setamountType(e.target.value)}>
+                      <select id='amountType' name='amountType' onChange={(e) => setAmountType(e.target.value)}>
                         <option value='one time'>one time</option>
                         <option value='per day'>per day</option>
                         <option value='per week'>per week</option>
@@ -116,8 +140,43 @@ export default observer(({ setShowNewListing }: Props) => {
 
                 <div className='form-container'>
                   <div style={{display: 'flex'}}>
-                    <div className='form-flex'>
-                        <label htmlFor='score1'>reveal score1</label>
+                    {/* <div className='form-flex'> */}
+                        {Array(
+                            user.userState.sync.settings.sumFieldCount
+                        )
+                            .fill(0)
+                            .map((_, i) => {
+                                return (
+                                    <div key={i}>
+                                        <label htmlFor={`score${i +1}`}>prove Score {i + 1}</label>
+                                        <input
+                                            style={{width: '6rem'}}
+                                            type='text'
+                                            id={`score${i + 1}`}
+                                            name={`score${i + 1}`}
+                                            value={proveData[i] ?? '0'}
+                                            onChange={(event) => {
+                                                if (
+                                                    !/^\d*$/.test(
+                                                        event.target.value
+                                                    )
+                                                )
+                                                    return
+                                                setProveData(() => ({
+                                                    ...proveData,
+                                                    [i]: event.target.value,
+                                                }))
+                                                if (i === 0) {setScore1(event.target.value)}
+                                                if (i === 1) {setScore2(event.target.value)}
+                                                if (i === 2) {setScore3(event.target.value)}
+                                                if (i === 3) {setScore4(event.target.value)}
+                                            }}
+                                        />
+                                    </div>
+                                )
+                            })}
+
+                        {/* <label htmlFor='score1'>reveal score1</label>
                         <input 
                           type='text' 
                           id='score1' 
@@ -133,8 +192,8 @@ export default observer(({ setShowNewListing }: Props) => {
                           onChange={(e) => setScore2(e.target.value)}
                           className='form-input-sm'
                         />
-                    </div>
-                    <div className='form-flex'>
+                    </div> */}
+                    {/* <div className='form-flex'>
                         <label htmlFor='score3'>reveal score3</label>
                         <input 
                           type='text' 
@@ -151,20 +210,40 @@ export default observer(({ setShowNewListing }: Props) => {
                           onChange={(e) => setScore4(e.target.value)}
                           className='form-input-sm'
                         />
-                    </div>
+                    </div> */}
                   </div>
                 </div>
 
                 <div style={{display: 'flex', justifyContent: 'center'}}>
-                  <Button>prove trust scores</Button>
-                  <input 
-                    type='submit'
-                    value='post'
-                    onClick={() => app.createNewListing(section, category, title, amount, amountType, description, posterId, score1, score2, score3, score4)}
-                  />
+                  <Button
+                    onClick={async () => {
+                      const proof = await user.proveData(
+                          proveData
+                      )
+                      setRepProof(proof)
+                    }}
+                  >
+                    prove trust scores
+                  </Button>
+                  {repProof.proof.length ? (
+                    <>
+                      {repProof.valid ? '✅' : '❌'}
+                    </>
+                  ) : null}
+                  {repProof.valid ? (
+                    <input 
+                      type='submit'
+                      value='POST'
+                      onClick={() => app.createNewListing(section, category, title, amount, amountType, description, posterId, score1, score2, score3, score4)}
+                    />
+                  ) : (
+                     <button className='blocked'>POST</button>
+                  )}
                 </div>
               </div>
             </div>
+
+            
           {/* </form> */}
           <button className='close-btn' onClick={() => setShowNewListing(false)}>X</button>
         </div>
--- packages/frontend/src/pages/Deal.tsx
+++ packages/frontend/src/pages/Deal.tsx
@@ -8,34 +8,167 @@ import './home.css'
 import Trustlist from '../contexts/Trustlist';
 import User from '../contexts/User'
 
+type ReqInfo = {
+  nonce: number
+}
+
+type ProofInfo = {
+  publicSignals: string[]
+  proof: string[]
+  valid: boolean
+}
+
 export default observer(() => {
 
-  const { id } = useParams()
-  console.log(id)
+  const { id }: any = useParams()
   const app = React.useContext(Trustlist)
   const user = React.useContext(User)
 
+  const [reqData, setReqData] = React.useState<{
+    [key: number]: number | string
+  }>({})
+  const [reqInfo, setReqInfo] = React.useState<ReqInfo>({ nonce: 0 })
+  const [proveData, setProveData] = React.useState<{
+      [key: number]: number | string
+  }>({})
+  const [repProof, setRepProof] = React.useState<ProofInfo>({
+      publicSignals: [],
+      proof: [],
+      valid: false,
+  })
+
+  const fieldType = (i: number) => {
+    if (i < user.sumFieldCount) {
+        return 'sum'
+    } else if (i % 2 === user.sumFieldCount % 2) {
+        return 'replace'
+    } else return 'timestamp'
+  }
+
   React.useEffect(() => {
     const loadData = async () => {
-      // await app.loadDealById(id)
+      await app.loadDealById(id)
     }
     loadData()
   }, [])
 
   const deal = app.listingsById.get(id)
 
+  if (!user.userState) {
+    return <div className="container">Loading...</div>
+}
+
   return (
     <div>
+      <hr style={{margin: '3rem'}}></hr>
       <div>{id}</div>
       {deal ? 
         <>
-          {deal._id}
-          {deal.title}
-          {deal.posterId}
-          {deal.responderId}
-          {/* {deal.dealOpened ?
+        <div>
+          {/* {deal._id} */}
+          {deal.title} / {deal.posterId} / {deal.responderId}
+          {deal.dealOpened ?
           <button onClick={() => app.updateDeal(id, 'close')}>complete deal and attest to interaction</button>  
-          : null } */}
+          : null }
+        </div>
+
+<hr style={{margin: '3rem'}}></hr>
+
+        <div>
+        <div className="action-container">
+                        <div className="icon">
+                            <h2>Change Data</h2>
+                            <Tooltip text="You can request changes to data here. The demo attester will freely change your data." />
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                justifyContent: 'flex-start',
+                            }}
+                        >
+                            {Array(
+                                user.userState.sync.settings.fieldCount
+                            )
+                                .fill(0)
+                                .map((_, i) => {
+                                    return (
+                                        <div key={i} style={{ margin: '4px' }}>
+                                            <p>
+                                                Data {i} ({fieldType(i)})
+                                            </p>
+                                            <input
+                                                value={reqData[i] ?? ''}
+                                                onChange={(event) => {
+                                                    if (
+                                                        !/^\d*$/.test(
+                                                            event.target.value
+                                                        )
+                                                    )
+                                                        return
+                                                    setReqData(() => ({
+                                                        ...reqData,
+                                                        [i]: event.target.value,
+                                                    }))
+                                                }}
+                                            />
+                                        </div>
+                                    )
+                                })}
+                        </div>
+                        <div className="icon">
+                            <p style={{ marginRight: '8px' }}>
+                                Epoch key nonce
+                            </p>
+                            <Tooltip text="Epoch keys are short lived identifiers for a user. They can be used to receive reputation and are valid only for 1 epoch." />
+                        </div>
+                        <select
+                            value={reqInfo.nonce ?? 0}
+                            onChange={(event) => {
+                                setReqInfo((v) => ({
+                                    ...v,
+                                    nonce: Number(event.target.value),
+                                }))
+                            }}
+                        >
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                        </select>
+                        <p style={{ fontSize: '12px' }}>
+                            Requesting data with epoch key:
+                        </p>
+                        <p
+                            style={{
+                                maxWidth: '650px',
+                                wordBreak: 'break-all',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                            }}
+                        >
+                            {user.epochKey(reqInfo.nonce ?? 0)}
+                        </p>
+
+                        <Button
+                            onClick={async () => {
+                                if (
+                                    user.userState &&
+                                    user.userState.sync.calcCurrentEpoch() !==
+                                        (await user.userState.latestTransitionedEpoch())
+                                ) {
+                                    throw new Error('Needs transition')
+                                }
+                                await user.requestReputation(
+                                    reqData,
+                                    reqInfo.nonce ?? 0
+                                )
+                                setReqData({})
+                            }}
+                        >
+                            Attest
+                        </Button>
+                    </div>
+        </div>
         </>
         : 'deal not found' }  
         
