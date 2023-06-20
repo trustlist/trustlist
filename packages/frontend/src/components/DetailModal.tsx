f3e2a674441a8d79d95c70b40c8dc88d685f09d8
CJ Rose <cjrose59@gmail.com>
2023-06-20 18:39:10 +0200

    fix detail modal score display

--- packages/frontend/src/components/DetailModal.tsx
+++ packages/frontend/src/components/DetailModal.tsx
@@ -54,7 +54,10 @@ export default observer(({ listing, setShowDetail }: Props) => {
   }, [])
   const offers = app.offersByListingId.get(listing._id)
   const memberKeys = [user.epochKey(0), user.epochKey(1), user.epochKey(2)]
-
+  const score1 = app.calcScore(listing.pScore1, false)
+  const score2 = app.calcScore(listing.pScore2, false)
+  const score3 = app.calcScore(listing.pScore3, false)
+  const score4 = app.calcScore(listing.pScore4, true)
   
 
   return (
@@ -65,7 +68,7 @@ export default observer(({ listing, setShowDetail }: Props) => {
             
             <div className='action-bar'>
               <div className='action-item'>
-                {/* uncomment this to prevent user from making an offer on their own post */}
+                {/* prevent user from making an offer on their own post */}
                 {/* {user.hasSignedUp && !memberKeys.includes(listing.posterId) ? ( */}
                 {user.hasSignedUp && !listing.dealOpened ? (
                   <>
@@ -108,28 +111,52 @@ export default observer(({ listing, setShowDetail }: Props) => {
                     text={`Legitimate Poster score: this member has completed a transaction for ${app.calcScore(listing.pScore1, false)}% of the listings they have posted.`}
                     content=' LP score : '
                   />
-                  <div style={{fontWeight: '600'}}>{app.calcScore(listing.pScore1, false)} %</div>
+                  <div style={{fontWeight: '600'}}>
+                    {score1 === 'X' ?
+                      <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                      :
+                      score1 === 0 ? '...' : <div>{score1} %</div>
+                    }
+                  </div>
                 </div>
                 <div className='trust-item'>
                   <Tooltip 
                     text={`Community Builder score: this member has submitted attestations for ${app.calcScore(listing.pScore2, false)}% of the transactions they have been involved in`}
                     content=' CB score : '
                   />
-                  <div style={{fontWeight: '600'}}>{app.calcScore(listing.pScore2, false)} %</div>
+                  <div style={{fontWeight: '600'}}>
+                    {score2 === 'X' ?
+                      <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                      :
+                      score2 === 0 ? '...' : <div>{score2} %</div>
+                    }
+                  </div>
                 </div>
                 <div className='trust-item'>
                   <Tooltip 
                     text={`Trusted DealMaker: ${app.calcScore(listing.pScore3, false)}% of members who have transacted with this member would be happy to deal with them again`} 
                     content=' TD score : '
                   />
-                  <div style={{fontWeight: '600'}}>{app.calcScore(listing.pScore3, false)} %</div>
+                  <div style={{fontWeight: '600'}}>
+                    {score3 === 'X' ?
+                      <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                      :
+                      score3 === 0 ? '...' : <div>{score3} %</div>
+                    }
+                  </div>
                 </div>
                 <div className='trust-item'>
                   <Tooltip 
                     text={`Good Vibes score : others who have interacted with his member have given them ${app.calcScore(listing.pScore4, true)}% of all possible points for being friendly, communicative, and respectful`} 
                     content=' GV score : '
                   />
-                  <div style={{fontWeight: '600'}}>{app.calcScore(listing.pScore4, true)} %</div>
+                  <div style={{fontWeight: '600'}}>
+                    {score4 === 'X' ?
+                      <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                      :
+                      score4 === 0 ? '...' : <div>{score4} %</div>
+                    }
+                  </div>
                 </div>
               </div>  
             </div>
@@ -141,29 +168,59 @@ export default observer(({ listing, setShowDetail }: Props) => {
                   <div style={{textDecoration: 'line-through'}}>pending offers</div>
                   <div style={{color: 'blue', paddingLeft: '1rem'}}>offer accepted</div>
                 </div>
-                  <div className='offer-scroll'>
+                <div className='offer-scroll'>
                   {offers ? 
                     offers.map((offer: Offer) => {
-                    
-                    <div key={offer._id} className='offer'>
-                      <div><span style={{color: 'blue'}}>${offer.offerAmount} </span>  ----    offering member's scores: </div>
-                      <div className='offer-score'><span style={{fontWeight: '300'}}>LP: </span>{app.calcScore(listing.pScore1, false)} </div>
-                      <div className='offer-score'><span style={{fontWeight: '300'}}>CB: </span>{app.calcScore(listing.pScore2, false)} </div>
-                      <div className='offer-score'><span style={{fontWeight: '300'}}>TD: </span>{app.calcScore(listing.pScore1, false)} </div>
-                      <div className='offer-score'><span style={{fontWeight: '300'}}>GV: </span>{app.calcScore(listing.pScore1, true)} </div> 
-                      {listing.responderId === offer.responderId ?
-                        <button className='offer-accepted'>accepted</button>
-                      : null }
-                      <hr/>
-                    </div>
-                  }) : 'no offers yet' }
-              
+                      const offerS1 = app.calcScore(offer.rScore1, false)
+                      const offerS2 = app.calcScore(offer.rScore2, false)
+                      const offerS3 = app.calcScore(offer.rScore3, false)
+                      const offerS4 = app.calcScore(offer.rScore4, true)
+                      return (
+                        <div key={offer._id} className='offer'>
+                          <div><span style={{color: 'blue'}}>${offer.offerAmount} </span>  ----    offering member's scores: </div>
+                          <div className='offer-score'>
+                            <span style={{fontWeight: '300'}}>LP: </span>
+                            {offerS1 === 'X' ?
+                              <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                              :
+                              offerS1 === 0 ? '...' : offerS1
+                            }
+                          </div>
+                          <div className='offer-score'>
+                            <span style={{fontWeight: '300'}}>CB: </span>
+                            {offerS2 === 'X' ?
+                              <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                              :
+                              offerS2 === 0 ? '...' : offerS2
+                            }
+                          </div>
+                          <div className='offer-score'>
+                            <span style={{fontWeight: '300'}}>TD: </span>
+                            {offerS3 === 'X' ?
+                              <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                              :
+                              offerS3 === 0 ? '...' : offerS3
+                            }
+                          </div>
+                          <div className='offer-score'>
+                            <span style={{fontWeight: '300'}}>GV: </span>
+                            {offerS4 === 'X' ?
+                              <img src={require('../../public/not_visible.svg')} alt="eye with slash"/>
+                              :
+                              offerS4 === 0 ? '...' : offerS4
+                            }
+                          </div>
+                          {listing.responderId === offer.responderId ?
+                            <button className='offer-accepted'>accepted</button>
+                          : null }
+                        </div>
+                  )}) : 'no offers yet' }
                 </div>
                 </>
               :
-              <>
-              <div style={{color: 'blue'}}>pending offers</div>
-              <div className='offer-scroll'>
+                <>
+                <div style={{color: 'blue'}}>pending offers</div>
+                <div className='offer-scroll'>
                   {offers ? 
                     offers.map((offer: Offer) => {
                       const offerS1 = app.calcScore(offer.rScore1, false)
@@ -220,7 +277,7 @@ export default observer(({ listing, setShowDetail }: Props) => {
                         </div>
                       )
                   }) : 'no offers yet' }
-              </div>
+                </div>
               </> 
               }
             </div>
