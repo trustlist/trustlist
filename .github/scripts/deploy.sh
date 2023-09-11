a400c91ccbf697bce3e5c5162b9632af2be4e379
Nick <nick@gmx.co.uk>
2023-09-11 14:45:42 +0300

    Fix service name

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,8 +5,9 @@ env=$1
  
 tasks="unirep-frontend-$env unirep-relayer-$env unirep-node-$env"
 for task in $tasks; do
+  service_name=$(echo $task | awk -F- '{ print $2 }')
   unirep_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
-  aws ecs update-service --cluster unirep-$env --service $task --force-new-deployment --task-definition $task:$unirep_revision
+  aws ecs update-service --cluster unirep-$env --service $service_name --force-new-deployment --task-definition $task:$unirep_revision
 done
 
 aws ecs wait services-stable --cluster unirep-$env --services $tasks
