d7a3c363e58fe32c367a8756232124428aeca871
Nick <nick@gmx.co.uk>
2023-09-18 11:17:46 +0300

    Change Unirep -> Trustlist

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,17 +8,17 @@ env=$2
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
-docker build -t unirep-frontend-$env -f apps/frontend/Dockerfile .
-docker tag unirep-frontend-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-frontend-$env:latest
-docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-frontend-$env:latest
+docker build -t trustlist-frontend-$env -f apps/frontend/Dockerfile .
+docker tag trustlist-frontend-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-frontend-$env:latest
+docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-frontend-$env:latest
 
-docker build -t unirep-relayer-$env -f apps/relayer/Dockerfile .
-docker tag unirep-relayer-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-relayer-$env:latest
-docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-relayer-$env:latest
+docker build -t trustlist-relayer-$env -f apps/relayer/Dockerfile .
+docker tag trustlist-relayer-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-relayer-$env:latest
+docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-relayer-$env:latest
 
-docker build -t unirep-node-$env -f apps/node/Dockerfile .
-docker tag unirep-node-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-node-$env:latest
-docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/unirep-node-$env:latest
+docker build -t trustlist-node-$env -f apps/node/Dockerfile .
+docker tag trustlist-node-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-node-$env:latest
+docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/trustlist-node-$env:latest
 
 exit 0
 exit 0
