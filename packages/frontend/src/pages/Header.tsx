9dbd11d689e90ebf88bf9d6720b430b5e8d81ee4
CJ Rose <cjrose59@gmail.com>
2023-10-20 14:35:29 -0700

    add toast to actions

--- packages/frontend/src/pages/Header.tsx
+++ packages/frontend/src/pages/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useNavigate } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import MemberDashboardModal from '../components/MemberDashboardModal'
 // import NewListingModal from '../components/NewListingModal'
@@ -8,203 +8,112 @@ import Tooltip from '../components/Tooltip'
 import './header.css'
 import { User2 } from 'lucide-react'
 import { Pencil } from 'lucide-react'
-import { PenSquare } from 'lucide-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import User from '../contexts/User'
 import Interface from '../contexts/interface'
 
 export default observer(() => {
-    const user = React.useContext(User)
-    const ui = React.useContext(Interface)
+  const user = React.useContext(User)
+  const ui = React.useContext(Interface)
+  const navigate = useNavigate()
 
-    const [remainingTime, setRemainingTime] = React.useState<number>(0)
-    const [showNewListing, setShowNewListing] = React.useState<boolean>(false)
-    const [showMemberDash, setShowMemberDash] = React.useState<boolean>(false)
+  const [remainingTime, setRemainingTime] = React.useState<number>(0)
+  // const [showNewListing, setShowNewListing] = React.useState<boolean>(false)
+  const [showMemberDash, setShowMemberDash] = React.useState<boolean>(false)
 
-    const updateTimer = () => {
-        if (!user.userState) {
-            setRemainingTime(0)
-            return
-        }
-        const time = user.userState.sync.calcEpochRemainingTime()
-        setRemainingTime(time)
+  const updateTimer = () => {
+    if (!user.userState) {
+      setRemainingTime(0)
+      return
     }
+    const time = user.userState.sync.calcEpochRemainingTime()
+    setRemainingTime(time)
+  }
 
-    React.useEffect(() => {
-        setInterval(() => {
-            updateTimer()
-        }, 1000)
-        if (showMemberDash || showNewListing) {
-            document.body.style.overflow = 'hidden'
-        } else {
-            document.body.style.overflow = 'auto'
-        }
-    }, [showMemberDash, showNewListing])
+  React.useEffect(() => {
+    setInterval(() => {
+      updateTimer()
+    }, 1000)
+    if (showMemberDash) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [showMemberDash])
 
-    const date = new Date()
-    date.setSeconds(date.getSeconds() + remainingTime)
-    const dateString = `${date.toDateString().slice(4)}  @  ${date.getHours()}:${date.getMinutes()}`
+  const date = new Date()
+  date.setSeconds(date.getSeconds() + remainingTime)
+  const dateString = `${date.toDateString().slice(4)}  @  ${date.getHours()}:${date.getMinutes()}`
 
-    return (
-        <>
-            {!ui.isMobile ? (
-                <div className="header">
-                    <div className="app-title">
-                        <Link to="/">trustlist</Link>
-                    </div>
+  const notify = () => toast.warning("You're not a member yet. Please click JOIN to participate.");
 
-                    <div className="epoch-info">
-                        <div className='flex items-center gap-2'>
-                          <div>
-                            epoch: {user.userState?.sync.calcCurrentEpoch()}
-                          </div>
-                          <Tooltip
-                              text='Trustlist epochs are 7 days long. Listings and their related offers and deals will expire at the close of each epoch. Members must transition to the new epoch in order to participate.'
-                              content={
-                                  <img
-                                      src={require('../../public/info_icon.svg')}
-                                      alt="info icon"
-                                  />
-                              }
-                          />
-                        </div>
-                        <div>
-                            ending:{' '}
-                            <span>{dateString}</span>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="header">
+        <div className={`${ui.isMobile ? null : 'flex'}`}>
+          <div className="app-title">
+            <Link to="/">trustlist</Link>
+          </div>
 
-                    <div className="actions">
-                        <div className="action-item">
-                            {!user.hasSignedUp ? (
-                                <Button onClick={() => user.signup()}>
-                                    JOIN
-                                </Button>
-                            ) : (
-                                <div>
-                                    <Button>connected</Button>
-                                </div>
-                            )}
-                        </div>
-                        <div className="action-item">
-                            {user.hasSignedUp ? (
-                                <>
-                                    <button
-                                        onClick={() => setShowMemberDash(true)}
-                                        className='px-3 py-1 font-semibold flex items-center gap-1'
-                                    >
-                                        <User2 color='blue'/>
-                                        <div>
-                                          <div>member</div>
-                                          <div>dashboard</div>
-                                        </div>
-                                    </button>
-                                    {showMemberDash && (
-                                        <MemberDashboardModal
-                                            setShowMemberDash={
-                                                setShowMemberDash
-                                            }
-                                        />
-                                    )}
-                                </>
-                            ) : (
-                                <button style={{ cursor: 'not-allowed' }}>
-                                    <span style={{ fontSize: '0.6rem' }}>
-                                        👤
-                                    </span>{' '}
-                                    my TL
-                                </button>
-                            )}
-                        </div>
-                        <div className="action-item">
-                            {user.hasSignedUp &&
-                                <Link to={'/listings/new'}>
-                                    <button className='px-3 py-1 font-semibold flex items-center gap-1'>
-                                      <Pencil color='blue'/>
-                                      <div>
-                                        <div>create</div>
-                                        <div>listing</div>
-                                      </div>
-                                    </button>
-                                </Link>
-                            }
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="header">
-                    <div>
-                        <div className="app-title">
-                            <Link to="/">trustlist</Link>
-                        </div>
-                        <div className="epoch-info">
-                            <div className='flex items-center gap-2'>
-                              <div>
-                                epoch: {user.userState?.sync.calcCurrentEpoch()}
-                              </div>
-                              <Tooltip
-                                  text='Trustlist epochs are 7 days long. Listings and their related offers and deals will expire at the close of each epoch. Members must transition to the new epoch in order to participate.'
-                                  content={
-                                      <img
-                                          src={require('../../public/info_icon.svg')}
-                                          alt="info icon"
-                                      />
-                                  }
-                              />
-                            </div>
-                            <div>ending:</div>
-                            <div>{dateString}</div>
-                        </div>
-                    </div>
+          <div className="epoch-info">
+            <div className='flex items-center gap-2'>
+              <div>
+                epoch: {user.userState?.sync.calcCurrentEpoch()}
+              </div>
+              <Tooltip
+                text='Trustlist epochs are 7 days long. Listings and their related offers and deals will expire at the close of each epoch. Members must transition to the new epoch in order to participate.'
+                content={
+                  <img
+                      src={require('../../public/info_icon.svg')}
+                      alt="info icon"
+                  />
+                }
+              />
+            </div>
+            <div className={`${ui.isMobile ? null : 'flex'}`}>
+              <div>ending:{' '}</div>
+              <div>{dateString}</div>
+            </div>
+          </div>
+        </div>
 
-                    <div className="header-buttons">
-                        {!user.hasSignedUp ? (
-                            <Button  style={{width: '100%'}} onClick={() => user.signup()}>JOIN</Button>
-                        ) : (
-                            <div>
-                                <Button style={{width: '100%'}}>connected</Button>
-                            </div>
-                        )}
-                        <div className="actions">
-                            {user.hasSignedUp ? (
-                                <>
-                                    <button
-                                        onClick={() => setShowMemberDash(true)}
-                                        className='px-3 py-1 font-semibold flex items-center gap-1 w-full'
-                                    >
-                                        <User2 color='blue'/>
-                                        <div>
-                                          <div>member</div>
-                                          <div>dashboard</div>
-                                        </div>
-                                    </button>
-                                    {showMemberDash && (
-                                        <MemberDashboardModal
-                                            setShowMemberDash={
-                                                setShowMemberDash
-                                            }
-                                        />
-                                    )}
-                                </>
-                            ) : (
-                                <button>👤</button>
-                            )}
-                            <div >
-                                {user.hasSignedUp &&
-                                    <Link to={'/listings/new'}>
-                                      <button className='px-3 py-2 font-semibold flex items-center gap-1 w-full'>
-                                        <Pencil color='blue'/>
-                                        <div>create lsiting</div>
-                                      </button>
-                                    </Link>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+        <div className="actions">
+          <Button 
+            onClick={() => user.hasSignedUp ? null : user.signup()}
+            loadingText='joining...'
+          >
+            {user.hasSignedUp ? 'connected' : 'JOIN'}
+          </Button>
 
-            <Outlet />
-        </>
-    )
+          <button
+            onClick={() => user.hasSignedUp ? setShowMemberDash(true) : notify()}
+            className='px-3 py-1 font-semibold flex justify-center items-center gap-1 text-sm'
+          >
+            <User2 color='blue'/>
+            <div>
+              <div>member</div>
+              <div>dashboard</div>
+            </div>
+          </button>
+          {showMemberDash && <MemberDashboardModal setShowMemberDash={setShowMemberDash}/>}
+            
+          <button 
+            onClick={() => user.hasSignedUp ? navigate('/listings/new') : notify()}
+            className='px-3 py-1 font-semibold flex justify-center items-center gap-2 text-sm'
+          >
+            <Pencil color='blue'/>
+            <div>
+              <div>create</div>
+              <div>listing</div>
+            </div>
+          </button> 
+        </div>
+      </div>
+
+      <ToastContainer position='top-right' autoClose={4000} />
+      <Outlet />
+    </>
+  )
 })
