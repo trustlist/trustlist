4b79bcf21945e94143395a5d2b6e359010d65c67
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-11-02 13:15:38 +0800

    fix(contracts): use auto-mining node

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -6,10 +6,18 @@ export default {
     networks: {
         hardhat: {
             blockGasLimit: 12000000,
+            mining: {
+                auto: true,
+                interval: 1000,
+            },
         },
         local: {
             url: 'http://127.0.0.1:8545',
             blockGasLimit: 12000000,
+            mining: {
+                auto: true,
+                interval: 1000,
+            },
             accounts: [
                 '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
             ],
