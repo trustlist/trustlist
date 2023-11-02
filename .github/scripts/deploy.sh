378447ef43c3097babeadc2fd1f3ac7cf3a404a7
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-11-02 10:53:37 +0800

    fix(ci): fix build order

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,7 +3,7 @@ set -ex
 
 env=$1
  
-tasks="trustlist-frontend-$env trustlist-relayer-$env trustlist-node-$env"
+tasks="trustlist-node-$env trustlist-relayer-$env trustlist-frontend-$env"
 for task in $tasks; do
   trustlist_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
   aws ecs update-service --cluster trustlist-$env --service $task --force-new-deployment --task-definition $task:$trustlist_revision
