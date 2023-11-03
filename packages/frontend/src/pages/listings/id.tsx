9042f8d845882d372fc1b100a1be8ebfb8c85609
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-11-03 01:02:05 -0500

    feat: :lipstick: Update Listing Details page
    Add layout and styles

--- packages/frontend/src/pages/listings/id.tsx
+++ packages/frontend/src/pages/listings/id.tsx
@@ -1,14 +1,16 @@
 
 import { Button } from '@/components/ui/button';
-import { TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import useTrustlist from '@/hooks/useTrustlist';
+import { ChevronRight } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface ListingProps {
   title: string;
   description: string;
   amount: string;
+  amountType: string;
   offers: {
     id: string,
     status: string,
@@ -31,14 +33,21 @@ interface ListingProps {
 
 const ListingDetails: React.FC = () => {
   const { id } = useParams();
-  const { getDeals } = useTrustlist();
+  const { getDeals, getOffers } = useTrustlist();
   const [loading, setLoading] = useState(true);
   const [listingDetails, setListingDetails] = useState<ListingProps | null>(null);
 
   useEffect(() => {
     const fetchListingDetails = async () => {
-      const response = await getDeals(id as string);
-      setListingDetails(response.data as any);
+      const listingResponse = await getDeals(id as string);
+      if (listingResponse.data) {
+        const offersResponse = await getOffers(id as string)
+        const listings = {
+          ...listingResponse.data,
+          offers: offersResponse.data
+        }
+        setListingDetails(listings as any);
+      }
       setLoading(false);
     };
 
@@ -46,23 +55,65 @@ const ListingDetails: React.FC = () => {
   }, [id]);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <p className='p-6'>Loading...</p>;
   }
 
   if (!listingDetails) {
-    return <p>No listing details available.</p>;
+    return <p className='p-6'>No listing details available.</p>;
   }
 
-  const { title, description, amount} = listingDetails;
-  console.log({...listingDetails});
+  const { title, description, amount, offers } = listingDetails;
+  console.log({ ...listingDetails });
 
   return (
-    <article className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-x-4">
-      <header>
-        <h1 className="text-3xl font-medium text-foreground">{title}</h1>
-        <p className="text-slate-500">{description}</p>
-        <p className="text-slate-500">${parseFloat(amount).toFixed(2)}</p>
-      </header>
+    <section className="flex flex-col space-y-6">
+      <article className='flex place-items-center md:justify-center border-b'>
+        <Link to='/' className='text-card-foreground m-0'><Button variant={'link'} size={'sm'} className='text-muted-foreground hover:text-foreground duration-300'>Home</Button></Link>
+        <ChevronRight size={14} className='text-muted-foreground' />
+        <p className='p-3'>{listingDetails.title}</p>
+      </article>
+
+      <section className="md:container md:max-w-3xl">
+        <section className='flex flex-col gap-2 container px-4 py-6 col-span-1 bg-primary text-secondary'>
+          <h1 className="text-4xl font-medium break-words" style={{ fontFamily: 'Times New Roman' }}>{title}</h1>
+          <p className="text-secondary text-lg">${parseFloat(amount).toFixed(2)} — {listingDetails.amountType}</p>
+          <p className="text-base text-secondary/70">{description}</p>
+        </section>
+
+        <section className='flex flex-col gap-3 col-span-1 p-4'>
+          <h2 className='text-lg font-semibold'>📃Listing Offers</h2>
+          {offers.length > 0 ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>ID</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>TrustScores</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {offers.map((offer) => (
+                  <TableRow key={offer.id} className={offer.status === 'accepted' ? 'text-green-500' : ''}>
+                    <TableCell>{offer.id.substring(0, 6)}...</TableCell>
+                    <TableCell>${offer.price.toFixed(2)}</TableCell>
+                    <TableCell>
+                      LP: {offer.trustScores.LP * 100}%<br />
+                      GV: {offer.trustScores.GV * 100}%<br />
+                      LO: {offer.trustScores.LO * 100}%<br />
+                      CB: {offer.trustScores.CB * 100}%
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <div className='flex flex-col gap-2'>
+              <p className='text-muted-foreground'>No offers available yet. You can be the first!</p>
+              <Button variant={'secondary'} className='self-start'>Make an offer</Button>
+            </div>
+          )}
+        </section>
+      </section>
       {/* <section>
         <p>LP: {trustScores.LP * 100}%</p>
         <p>GV: {trustScores.GV * 100}%</p>
@@ -70,40 +121,7 @@ const ListingDetails: React.FC = () => {
         <p>CB: {trustScores.CB * 100}%</p>
       </section> */}
 
-      {/* <section>
-        <h2>Offers</h2>
-        {offers.length > 0 ? (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>ID</TableHead>
-                <TableHead>Price</TableHead>
-                <TableHead>TrustScores</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {offers.map((offer) => (
-                <TableRow key={offer.id} className={offer.status === 'accepted' ? 'text-green-500' : ''}>
-                  <TableCell>{offer.id.substring(0, 6)}...</TableCell>
-                  <TableCell>${offer.price.toFixed(2)}</TableCell>
-                  <TableCell>
-                    LP: {offer.trustScores.LP * 100}%<br />
-                    GV: {offer.trustScores.GV * 100}%<br />
-                    LO: {offer.trustScores.LO * 100}%<br />
-                    CB: {offer.trustScores.CB * 100}%
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        ) : (
-          <div>
-            <p>No offers available at the moment.</p>
-            <Button >Submit an Offer</Button>
-          </div>
-        )}
-      </section> */}
-    </article>
+    </section>
   );
 };
 
