0b806cb853edcbe72a80141f5d803a61222de00f
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-11-07 11:32:51 -0500

    feat: :lipstick: Update `Deal` styles
    Add pending review message, better conditionals for styles

--- packages/frontend/src/components/Deal.tsx
+++ packages/frontend/src/components/Deal.tsx
@@ -1,6 +1,7 @@
 import User from "@/contexts/User";
 import useTrustlist from "@/hooks/useTrustlist";
 import { ListingProps } from "@/pages/listings/id";
+import { cn } from "@/utils/cn";
 import { MoveHorizontal, MoveVertical } from "lucide-react";
 import { useContext } from "react";
 import ReviewForm from "./ReviewForm";
@@ -12,26 +13,28 @@ interface ListingPropsFromDetails {
 
 const Deal = ({ listing }: ListingPropsFromDetails) => {
   const { closeDeal } = useTrustlist()
-  const user = useContext(User)
+  const user = useContext(User);
   const memberKeys = [user.epochKey(0), user.epochKey(1), user.epochKey(2)]
   const { _id, amount, offerAmount, posterId, contact, responderId, posterDealClosed, responderDealClosed, posterReview, responderReview } = listing;
 
   return (
-    <section className="border border-primary rounded-sm p-4">
+    <section className={cn("border border-primary rounded-sm p-4", posterDealClosed && responderDealClosed ? 'border-green-600 border-[1.5px]' : '')}>
       {(!posterDealClosed || !responderDealClosed)
-        ? <div className='flex text-lg pb-4'>
-          <p className="text-xs text-orange-600 tracking-wider py-1 px-2 border border-orange-500 uppercase rounded-md self-start font-semibold">PENDING DEAL</p>
+        ? <div className='text-lg pb-4'>
+          <p className="text-xs text-primary tracking-wider py-1 px-2 border border-primary uppercase rounded-md self-start font-semibold">DEAL OPEN</p>
           {memberKeys.includes(responderId) ? <p>contact: {contact}</p> : null}
         </div>
         : null}
       {posterDealClosed && responderDealClosed
-        ? <p className="text-xs text-green-600 tracking-wider py-1 px-2 border border-green-500 uppercase rounded-md self-start font-semibold">DEAL COMPLETED</p>
+        ? <p className="text-xs text-green-50 tracking-wider py-1 px-2 border bg-green-500 uppercase rounded-md w-fit font-semibold">
+          DEAL COMPLETED
+        </p>
         : null}
 
-      <article className='flex flex-col md:flex-row gap-4 justify-between'>
+      <article className='flex flex-col md:flex-row gap-4 justify-between py-2'>
         <section>
           <p className="text-muted-foreground font-semibold uppercase text-sm tracking-wider">Poster</p>
-          <p className="text-card-foreground text-2xl">${parseFloat(amount).toFixed(2)}</p>
+          <p className="text-card-foreground font-bold text-2xl">${parseFloat(amount).toFixed(2)}</p>
           {posterDealClosed ?
             <Button disabled variant={'secondary'}>Poster approved</Button>
             :
@@ -61,22 +64,24 @@ const Deal = ({ listing }: ListingPropsFromDetails) => {
                   Approve
                 </Button>
                 :
-                <Button disabled variant={'secondary'}>
+                <Button disabled variant={'secondary'} size={'sm'}>
                   Waiting on poster approval...
                 </Button>
               }
             </div>
           }
+          {posterDealClosed && responderDealClosed && !posterReview
+            ? (<p className="text-orange-500 py-2">Awaiting poster review...</p>) : null}
         </section>
         <MoveVertical className="block md:hidden text-primary" size={32} />
-        <MoveHorizontal className="hidden md:block text-primary"  size={32}/>
+        <MoveHorizontal className="hidden md:block text-primary" size={32} />
         <section>
           <p className="text-muted-foreground font-semibold uppercase text-sm tracking-wider">buyer</p>
-          <p className="text-green-700 text-2xl">${parseFloat(offerAmount).toFixed(2)}</p>
+          <p className="text-green-700 font-bold text-2xl">${parseFloat(offerAmount).toFixed(2)}</p>
           {responderDealClosed ?
             <Button disabled variant={'secondary'}>Buyer approved</Button>
             :
-            <div className="">
+            <div>
               {memberKeys.includes(responderId)
                 ? <Button
                   onClick={async () => {
@@ -102,39 +107,41 @@ const Deal = ({ listing }: ListingPropsFromDetails) => {
                   Approve
                 </Button>
                 :
-                <Button variant={'secondary'} disabled>
-                  Waiting on buyer approval
+                <Button variant={'secondary'} size={'sm'} disabled>
+                  Waiting on buyer approval...
                 </Button>
               }
             </div>
           }
+          {posterDealClosed && responderDealClosed && !responderReview
+            ? (<p className="text-orange-600 py-2">Awaiting responder review...</p>) : null}
         </section>
-
-        {posterDealClosed && responderDealClosed ?
-          <div className="container">
-            {memberKeys.includes(posterId) && (<ReviewForm
-              key={posterId}
-              dealId={_id}
-              member="poster"
-              memberKeys={memberKeys}
-              currentMemberId={posterId}
-              oppositeMemberId={responderId}
-              currentMemberReview={posterReview}
-              oppositeMemberReview={responderReview}
-            />)}
-            {memberKeys.includes(responderId) && (<ReviewForm
-              key={responderId}
-              dealId={_id}
-              member="responder"
-              memberKeys={memberKeys}
-              currentMemberId={responderId}
-              oppositeMemberId={posterId}
-              currentMemberReview={responderReview}
-              oppositeMemberReview={posterReview}
-            />)}
-          </div> : null
-        }
       </article>
+      <div className="p-2"></div>
+      {posterDealClosed && responderDealClosed ?
+        <div className="w-full">
+          {memberKeys.includes(posterId) && (<ReviewForm
+            key={posterId}
+            dealId={_id}
+            member="poster"
+            memberKeys={memberKeys}
+            currentMemberId={posterId}
+            oppositeMemberId={responderId}
+            currentMemberReview={posterReview}
+            oppositeMemberReview={responderReview}
+          />)}
+          {memberKeys.includes(responderId) && (<ReviewForm
+            key={responderId}
+            dealId={_id}
+            member="responder"
+            memberKeys={memberKeys}
+            currentMemberId={responderId}
+            oppositeMemberId={posterId}
+            currentMemberReview={responderReview}
+            oppositeMemberReview={posterReview}
+          />)}
+        </div> : null
+      }
     </section>
   )
 }
